7f2142d9cab7f7aa6b897195a8eecd1fc439effb
Chandru-Ghub <chandruinfo455@gmail.com>
2023-09-19 23:43:57 +0530

    added1

--- src/Project.js
+++ src/Project.js
@@ -11,16 +11,18 @@ const Project = () => {
     <h3 className='myworks' data-aos="fade-right" data-aos-duration="1500" >My works</h3>
     <div className='sliderContainer'>
             <SliderComp/>
-    </div>   
-    <h4 className='pro15'>15+ projects</h4>
-    <p className='moreWork'>If you have time have a look <span> THE WAY I WALK THROUGH </span></p>
-        <div className='lookup cv'>
- 
-            <button className='btncv'>
-                 <Link to='/dig' style={{color:'black'}}>More Works</Link>
-             </button>     
-            
-        </div>
+    </div> 
+      <div className="morepro">
+              <h4 className='pro15'>15+ projects</h4>
+              <p className='moreWork'>If you have time have a look <span> THE WAY I WALK THROUGH </span></p>
+                  <div className='lookup cv'>
+          
+                      <button className='btncv'>
+                          <Link to='/dig' style={{color:'black'}}>More Works</Link>
+                      </button>     
+                      
+                  </div>
+      </div>  
         {/* <p className='line1'></p> */}
     </div>
   )
